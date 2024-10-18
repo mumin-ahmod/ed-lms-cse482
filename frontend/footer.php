255af74fc1c82bfca61acc8ec22b7bc0d3e83b76
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-10-18 21:54:49 +0600

    footer.php

---
+++ frontend/footer.php
@@ -0,0 +1,53 @@
+<footer class="bg-dark text-white pt-5 pb-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+            <!-- About Section -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">World Education</h5>
+                <p>
+                    The World Education website is a comprehensive platform designed to enhance the learning experience for students and educators alike.
+                </p>
+            </div>
+
+            <!-- Quick Links -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Quick Links</h5>
+                <p><a href="index.php" class="text-white" style="text-decoration: none;">Home</a></p>
+                <p><a href="dashboard.php" class="text-white" style="text-decoration: none;">Dashboard</a></p>
+                <p><a href="explore.php" class="text-white" style="text-decoration: none;">Explore</a></p>
+                <p><a href="contact.php" class="text-white" style="text-decoration: none;">Contact</a></p>
+                <p><a href="admin-dashboard.php" class="text-white" style="text-decoration: none;">Admin Login</a></p>
+            </div>
+
+            <!-- Contact Section -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
+                <p><i class="fa fa-home mr-3"></i> Banani, Dhaka-1213, Bangladesh</p>
+                <p><i class="fa fa-envelope mr-3"></i> edlms@gmail.com</p>
+                <p><i class="fa fa-phone mr-3"></i> +880 019 7130 6441</p>
+                <p><i class="fa fa-print mr-3"></i> +880 017 7890 2***</p>
+            </div>
+
+            <!-- Social Media Links -->
+            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Follow Us</h5>
+                <p>
+                    <a href="#" class="text-white" style="text-decoration: none;"><i class="fa fa-facebook-f fa-lg mr-4"></i></a>
+                    <a href="#" class="text-white" style="text-decoration: none;"><i class="fa fa-twitter fa-lg mr-4"></i></a>
+                    <a href="#" class="text-white" style="text-decoration: none;"><i class="fa fa-instagram fa-lg mr-4"></i></a>
+                    <a href="#" class="text-white" style="text-decoration: none;"><i class="fa fa-linkedin fa-lg"></i></a>
+                </p>
+            </div>
+        </div>
+
+        <!-- Horizontal Line -->
+        <hr class="mb-4">
+
+        <!-- Copyright Section -->
+        <div class="row align-items-center">
+            <div class="col-md-7 col-lg-8">
+                <p>&copy; 2024 My PHP Project. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
